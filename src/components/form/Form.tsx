7d9a3c6bc97c5282ade6125de3bdf7c3f3d55b77
armen.asatryan <armen.asatryan@monitis.com>
2021-01-22 21:34:42 +0400

    asdasd

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,85 +1,14 @@
 import React, { FC } from 'react';
-import InputBox from '../inputBox/InputBox';
-import CheckBox from '../checkBox/CheckBox';
-import RadioButton from '../radioButton/RadioButton';
-import Select from '../selectOption/SelectOption';
-
-import './Form.css'
-
-// interface RadioButtonProps {
-//     wrapperClass: string;
-// }
+import './Form.css';
 
 export const Form: FC = () => {
-
     return (
-        <div className="form">
-           <div className="form-title">
-                <span>Create a new account</span>
-            </div>
-            <FormItem />
-        </div>
+        <div className="form" />
     );
 };
 
 export const FormItem: FC = () => {
-
     return (
-        <div>
-            <div className="form-item-input">
-                <InputBox 
-                    className="input-box-name"
-                    placeholder="Enter your name"
-                    name="name"
-                    customErrorText="Custom Error Text"
-                    isRequired={true}
-                />
-                <InputBox
-                    className="input-box-email"
-                    placeholder="Email"
-                    name="email"
-                    customErrorText="Custom Error Text"
-                    isRequired={true}
-                />
-                <InputBox
-                    className="input-box-pswd"
-                    placeholder="Password"
-                    name="password"
-                    customErrorText="Custom Error Text"
-                    isRequired={true}
-                />
-            </div>
-            <div>
-                <Select 
-                    options={[]}
-                    onSelect={ (): void => void 0 }
-                />
-            </div>
-
-            <RadioButton 
-                wrapperClass="form-item-radio"
-                disabled={ false }
-                checked={ false }
-                onclick={ (): void => void 0 }
-                label="male"
-                value="male"
-            />
-            <RadioButton 
-                wrapperClass="form-item-radio"
-                disabled={ false }
-                checked={ false }
-                onclick={ (): void => void 0 }
-                label="female"
-                value="female"
-            />
-
-            <CheckBox
-                wrapperClass='form-item-checkbox'
-                value="value"
-                label="Accept terms and conditions"
-                checked={ false }
-                onchange={ (): void => void 0 }
-            />
-        </div>
+        <div className='form-item' />
     );
 };
--- src/containers/SignUp.tsx
+++ src/containers/SignUp.tsx
@@ -1,5 +1,10 @@
 import React, { FC } from 'react';
-import { Form } from '../components/form/Form';
+import { Form, FormItem } from '../components/form/Form';
+import InputBox from '../components/inputBox/InputBox';
+import CheckBox from '../components/checkBox/CheckBox';
+import RadioButton from '../components/radioButton/RadioButton';
+import Select from '../components/selectOption/SelectOption';
+import Button from '../components/button/Button';
 
 import './SignUp.css';
 
@@ -7,7 +12,78 @@ export const SignUp: FC = () => {
 
     return (
         <div className={'container'}>
-            <Form />
+            <Form>
+                <FormItem>
+                    <InputBox 
+                        className="input-box-name"
+                        placeholder="Enter your name"
+                        name="name"
+                        customErrorText="Custom Error Text"
+                        isRequired={true}
+                    />
+                </FormItem>
+                <FormItem>
+                    <InputBox
+                        className="input-box-email"
+                        placeholder="Email"
+                        name="email"
+                        customErrorText="Custom Error Text"
+                        isRequired={true}
+                    />
+                </FormItem>
+                <FormItem>
+                    <InputBox
+                        className="input-box-pswd"
+                        placeholder="Password"
+                        name="password"
+                        customErrorText="Custom Error Text"
+                        isRequired={true}
+                    />
+                </FormItem>
+                <FormItem>
+                    <Select 
+                        options={[]}
+                        onSelect={ (): void => void 0 }
+                    />
+                </FormItem>
+                <FormItem>
+                        <RadioButton 
+                        wrapperClass="form-item-radio"
+                        disabled={ false }
+                        checked={ false }
+                        onclick={ (): void => void 0 }
+                        label="male"
+                        value="male"
+                    />
+                </FormItem>
+                <FormItem>
+                    <RadioButton 
+                        wrapperClass="form-item-radio"
+                        disabled={ false }
+                        checked={ false }
+                        onclick={ (): void => void 0 }
+                        label="female"
+                        value="female"
+                    />
+                </FormItem>
+                <FormItem>
+                    <CheckBox
+                        wrapperClass='form-item-checkbox'
+                        value="value"
+                        label="Accept terms and conditions"
+                        checked={ false }
+                        onchange={ (): void => void 0 }
+                    />
+                </FormItem>
+                <FormItem>
+                    <Button
+                         disabled={false}
+                         onclick={(): void => void 0 }
+                         title="title"
+                    />
+                </FormItem>
+
+            </Form>
         </div>
     );
 };
